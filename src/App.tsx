f2242d4f7db31d0690f271c58c0d44c5d6bdf56f
heydrigh <heydrigh@gmail.com>
2021-05-21 11:17:14 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
+import Routes from "./routes/index";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Routes />
+			<GlobalStyles />
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+import Button from "../../components/Button";
+
+const Home = () => (
+	<S.Wrapper>
+		<h1>Home</h1>
+		<Button>Test</Button>
+	</S.Wrapper>
+);
+
+export default Home;
